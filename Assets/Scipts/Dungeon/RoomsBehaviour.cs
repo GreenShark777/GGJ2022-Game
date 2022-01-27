681589a752c4517fdabfda8644b6b1ab878d796d
GreenShark777 <greenshark1740@gmail.com>
2022-01-27 01:53:33 +0100

    Aggiunte funzioni per le stanze
    Le stanze hanno 2 funzioni che verranno in seguito utilizzate dai nemici. Una da come riferimento ai nemici la stessa stanza, l'altra informa alla porta di controllo che un nemico ha perso la maschera

--- Assets/Scipts/Dungeon/RoomsBehaviour.cs
+++ Assets/Scipts/Dungeon/RoomsBehaviour.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 
 public class RoomsBehaviour : MonoBehaviour
 {
-    //riferimento alla porta d'entrata della stanza
+    
     [SerializeField]
-    private Transform entrance = default;
+    private Transform entrance = default, //riferimento alla porta d'entrata della stanza
+        enemiesContainer = default; //riferimento al contenitore dei nemici
+
     //riferimento alla porta che controlla se ci sono nemici nella stanza
     [SerializeField]
     private DoorsBehaviour checkingDoor = default;
@@ -13,11 +15,35 @@ public class RoomsBehaviour : MonoBehaviour
     private int roomID = -1;
 
 
+    private void Awake()
+    {
+        //informa i nemici della stanza in cui si trovano
+        InformEnemiesOfTheirRoom();
+
+
+
+        //DEBUG PER RICORDARE---------------------------------------------------------------------------------------------------------------
+        InformDoorOfMaskBreaking();
+
+    }
+
     /// <summary>
     /// Imposta il nuovo ID di questa stanza
     /// </summary>
     /// <param name="newID"></param>
     public void SetRoomID(int newID) { roomID = newID; }
+
+    public void InformEnemiesOfTheirRoom()
+    {
+
+        //BISOGNA DARE AI NEMICI QUESTO SCRIPT COME RIFERIMENTO, IN MODO CHE QUANDO VENGONO SCONFITTI POSSANO DICHIARARLO ALLA STESSA STANZA
+        //CHE A SUA VOLTA INFORMERA' LA PORTA DI CONTROLLO
+
+        //var allEnemies = enemiesContainer.GetComponentsInChildren<PetrificationVulnerability>();
+
+        Debug.LogWarning("DA RICORDARE: I NEMICI NON VENGONO ANCORA INFORMATI DELLA STANZA IN CUI SONO");
+
+    }
     /// <summary>
     /// Si occupa di ciò che deve accadere quando il giocatore entra in questa stanza
     /// </summary>
@@ -29,6 +55,15 @@ public class RoomsBehaviour : MonoBehaviour
         //fa controllare alla porta di controllo se ci sono nemici nella stanza
         checkingDoor.CheckForEnemiesWithMasks();
 
+    }
+    /// <summary>
+    /// Informa la porta di controllo che un nemico ha perso la maschera
+    /// </summary>
+    public void InformDoorOfMaskBreaking()
+    {
+        
+        checkingDoor.RemovedMaskOfAnEnemy();
+        Debug.LogWarning("DA RICORDARE: LA PORTA DI COTROLLO NON VIENE ANCORA INFORMATA QUANDO UNA MASCHERA VIENE ROTTA");
     }
     /// <summary>
     /// Ritorna l'ID di questa stanza
